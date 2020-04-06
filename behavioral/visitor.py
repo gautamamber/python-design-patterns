83bdc35312527ee5a79207148874e616c2326c04
gautamamber <ambergautam1@gmail.com>
2020-04-06 12:06:18 +0530

    feat():add visitor pattern

--- behavioral/visitor.py
+++
@@ -1,9 +0,0 @@
-"""
-Visitor Design patterns: It is one of the Behavioral design pattern.
-It separate the algorithm from an object structure on which it operates
-It helps us to add new features to an existing class hierarchy dynamically without changing it.
-A visitor pattern consist two parts:
-1. method name visit() implemented by the visitor and used and called
-for every element of the data structure
-2. Visitable classes providing Accept() methods that accept a visitor
-"""
--- creational/prototype.py
+++ creational/prototype.py
@@ -4,3 +4,45 @@ the number of classes for an application. It allows you to copy
 existing objects independent of the concrete implementation of their classes.
 The object is created by copying a prototypical instance during run-time.
 """
+
+
+class Prototype:
+
+    value = "default"
+
+    def clone(self, **attrs):
+        """
+        clone prototype
+        :return:
+        """
+        obj = self.__class__()
+        obj.__dict__.update(attrs)
+        return obj
+
+
+class PrototypeDispatcher:
+
+    def __init__(self):
+        self.objects = {}
+
+    def get_objects(self):
+        """
+        Get all objects
+        :return:
+        """
+        return self.objects
+
+    def register_objects(self, name, obj):
+        """
+        Register an object
+        :return:
+        """
+        self.objects[name] = obj
+
+    def unregister_object(self, name):
+        """
+        Unregister object
+        :param name:
+        :return:
+        """
+        del self.objects[name]
